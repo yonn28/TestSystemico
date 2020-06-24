c503612fc0a3e1725e2e8c78b2021ef79b737fcc
yonny <yonny.nova@ixev.com>
2020-06-23 23:28:28 -0500

    full program

--- test.py
+++ test.py
@@ -1,5 +1,31 @@
 import requests
+import json
 
 response = requests.get('https://randomuser.me/api/?results=100')
 
-print(response.json())
+data = response.json()
+results = data.get('results')
+
+male = []
+female = []
+
+for info in results:
+    if(info.get('gender') == 'male'):
+        male.append(info)
+    else:
+        female.append(info)
+
+def get_age(elem):
+    return elem['dob']['age']
+
+def get_email(elem):
+    return elem['email']
+
+
+male.sort(key=get_age)
+print(male, end='\n\n')
+
+
+female.sort(key=get_email,reverse=True)
+print(female, end='\n\n')
+
